be6f2f561d48ce79c8f5b213c6bf70f5c5833a9e
Grisel Matos <griselmatosm@gmail.com>
2020-05-26 13:02:18 +0200

    Add SongDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { selectSong } from '../actions';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 function App() {
   
@@ -9,6 +10,9 @@ function App() {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
